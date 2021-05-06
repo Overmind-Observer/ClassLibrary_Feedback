d20167b985d1ff757ae59600171591a92b8a8fc7
Action <ismailov_mr@mail.ru>
2021-05-07 02:26:15 +1200

    cosmeticchanges, writing mistakes fixes

--- Feedback.cs
+++ Feedback.cs
@@ -14,6 +14,11 @@ namespace ClassLibrary_Feedback
 
         public string UserName { get; private set; }
         public string Email { get; private set; }
+        public string Text { get; private set; }
+        public int Raiting { get; private set; }
+
+        public DateTime Date = DateTime.UtcNow;
+
         // Email validation
         private bool IsValid(string Email)
         {
@@ -21,21 +26,14 @@ namespace ClassLibrary_Feedback
             Match isMatch = Regex.Match(Email, pattern, RegexOptions.IgnoreCase);
             return isMatch.Success;
         }
-        public string Text { get; private set; }
-        public int Raiting { get; private set; }
-
-        public DateTime Date = DateTime.UtcNow;
-
-
         /// <summary>
         /// Check the Feedback state
         /// </summary>
         public void Sending()
         {
             _ = UserName.Length <= 20 && UserName != null ? IsSent = true : IsSent = false;
-            _ = Email != null ? IsSent = true : IsSent = false;
             _ = Raiting <=5 && Raiting >=1 ? IsSent = true : IsSent = false;
-            _ = IsSent != true ? "please submet your feedback" : "you have already sent your feedback";
+            _ = IsSent != true ? "please submit your feedback" : "you have already sent your feedback";
         }
     }
 }
