b3b248dcac270985688f597bbf45d238cc9cb07a
Action <ismailov_mr@mail.ru>
2021-05-08 11:18:57 +1200

    refactoring fixes and checker for message delivery

--- Feedback.cs
+++ Feedback.cs
@@ -4,77 +4,76 @@ using System.Net.Mail;
 
 namespace ClassLibrary_Feedback
 {
+
+
+    
     class Feedback
     {
+        // Feedback state
+        private bool IsSent = false;
+
         // Declair parameters
-
-        private string UserName;
-        public string username
+        // User name with lenght check (no more than 20 char) and can not be empty check
+        private string _UserName;
+        public string UserName
         {
-            get => UserName;
-            set => _ =UserName == null || UserName.Length > 20 ? throw new Exception("too long or empty name") :    
-            UserName = value;
+            get => _UserName;
+            set => _ = _UserName == "" || _UserName.Length > 20 ? throw new Exception("too long or empty name") :
+            _UserName = value;
         }
-
-        private string Email;
-        public string email
+        // Email with email validation (in progress)
+        private string _Email;
+        public string Email
         {
-            get => Email;
+            get => _Email;
 
 
             set
             {
-#pragma warning disable CS8321 // Local function is declared but never used
-                bool IsEmailValid(string Email)
-#pragma warning restore CS8321 // Local function is declared but never used
+                bool IsValidEmail(string _Email)
                 {
-                    try
-                    {
-                        var addr = new System.Net.Mail.MailAddress(Email);
-                        return addr.Address == Email;
-                    }
-                    catch
-                    {
-                        return false;
-                    }
+                    MailAddress addr = new MailAddress(_Email);
+                    return addr.Address == _Email;
                 }
-                Email = value;
             }
         }
-
-
-
-        private string Text;
-        public string text
+        // Message text can be empty
+        private string _Text;
+        public string Text
         {
-            get => Text;
-            set => Text = value;
+            get => _Text;
+            set => _Text = "";
         }
 
-        private int Raiting;
+        // Raiting with value (1 - 5) check
+        private int _Raiting;
 
-        public int raiting
+        public int Raiting
         {
-            get => Raiting;
-            set => _ = Raiting < 1 || Raiting > 5 ? throw new Exception("wrong value") :
-                Raiting = value;
+            get => _Raiting;
+            set => _ = _Raiting < 1 || _Raiting > 5 ? throw new Exception("incorrect value of Raiting") :
+                _Raiting = value;
         }
+        // Getting date
+        private DateTime _Date;  
+        public DateTime Date { 
+            get => _Date;
+            set => _Date = DateTime.Now;
+        } // not sure about this statement !?
 
-        private DateTime Date;
 
-        public DateTime date
-        {
-            get => Date;
-            set => Date = value;
-        }
         // sending the feedback
 
         public void Send()
         {
             MailAddress from = new MailAddress("UltimateQuarry@gmail.com");
             MailAddress to = new MailAddress("UQ_SupporTeam@gmail.com");
-            MailMessage msg = new MailMessage(from, to);
-            msg.Body = "From: " + username + ", Contact Email:" + email + ". Feedback" + text + ", Service rating: " + raiting + ", Date: " + date;
+            MailMessage message = new MailMessage(from, to)
+            {
+                Body = "From: " + UserName + ", Contact Email:" + Email
+                + ". Feedback" + Text + ", Service rating: " + Raiting
+                + ", Date: " + Date
+            };
 
             SmtpClient smtpClient = new SmtpClient
             {
@@ -85,7 +84,17 @@ namespace ClassLibrary_Feedback
                 UseDefaultCredentials = false,
                 Credentials = new NetworkCredential(from.Address, "UQtest0!")
             };
-            smtpClient.Send(msg);
+
+            try
+            {
+                smtpClient.Send(message); // will it work ?
+                IsSent = true;
+            }
+            catch
+            {
+                throw new Exception("your message was not send");
+            }
+           
         }
     }
 }
