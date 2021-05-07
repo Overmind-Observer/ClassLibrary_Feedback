b7d73db25e158c06b84adcdd841a120a4d145416
Action <ismailov_mr@mail.ru>
2021-05-07 21:10:44 +1200

    code changed new version of setters

--- Feedback.cs
+++ Feedback.cs
@@ -7,45 +7,80 @@ namespace ClassLibrary_Feedback
 {
     class Feedback
     {
-        
-        // Fedback states
-        
-        private bool IsSent;
 
-        // Getting parameters
+        // Fedback states
+
+#pragma warning disable IDE0052 // Remove unread private members
+        private bool IsSent = false;
+#pragma warning restore IDE0052 // Remove unread private members
+
+
+        // Declair parameters
 
         private string UserName;
+        public string username
+        {
+            get { return UserName; }
+            set
+            {
+                _ = value != null && value.Length <= 20;
+                UserName = value; 
+            }
+        }
+
         private string Email;
+        public string email
+        {
+            get { return Email; }
+
+
+            set {
+#pragma warning disable CS8321 // Local function is declared but never used
+                bool IsEmailValid(string Email)
+#pragma warning restore CS8321 // Local function is declared but never used
+                {
+                    try
+                    {
+                        var addr = new System.Net.Mail.MailAddress(Email);
+                        return addr.Address == Email;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                Email = value;
+            }
+        }
+
+
+
         private string Text;
+        public string text
+        {
+            get { return Text; }
+            set { Text = value; }
+        }
+
         private int Raiting;
+
+        public int raiting
+        {
+            get { return Raiting; }
+            set 
+            {
+                _ = value >= 1 && value <= 5;
+                Raiting = value;
+            }
+        }
+
         private DateTime Date;
 
-        // Checking Name lenght (no more then 20 char)
-        public void SetUserName(string UserName)
+        public DateTime date
         {
-            _ = UserName != null && UserName.Length <= 20;
+            get { return Date; }
+            set { Date = value; }
         }
-        // Checking Email validation
-        public bool SetEmailValid(string Email)
-        {
-                try
-                {
-                    var addr = new System.Net.Mail.MailAddress(Email);
-                    return addr.Address == Email;
-                }
-                catch
-                {
-                    return false;
-                }
-        }
-        // Setting Raiting range between 1 and 5
-        public void SetRaitingRule(int Raiting)
-        {
-            _ = Raiting >= 1 && Raiting <= 5;
-        }
-
-
-
         // sending the feedback
 
         public void Send()
@@ -53,7 +88,7 @@ namespace ClassLibrary_Feedback
             MailAddress from = new MailAddress("UltimateQuarry@gmail.com");
             MailAddress to = new MailAddress("UQ_SupporTeam@gmail.com");
             MailMessage msg = new MailMessage(from, to);
-            msg.Body = "From: "+ UserName + ", Contact Email:" + Email + ". Feedback" + Text + ", Service rating: " + Raiting + ", Date: " + Date;
+            msg.Body = "From: " + username + ", Contact Email:" + email + ". Feedback" + text + ", Service rating: " + raiting + ", Date: " + date;
 
             SmtpClient smtpClient = new SmtpClient
             {
@@ -64,6 +99,9 @@ namespace ClassLibrary_Feedback
                 UseDefaultCredentials = false,
                 Credentials = new NetworkCredential(from.Address, "UQtest0!")
             };
+            smtpClient.Send(msg);
+
+            IsSent = true;
         }
     }
 }
