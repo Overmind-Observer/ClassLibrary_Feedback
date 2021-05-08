d3d605112466cd523a7a31bb872b2366ca34e7e3
Action <ismailov_mr@mail.ru>
2021-05-08 22:05:32 +1200

    gitignore changed back corrections in the test section update

---
+++ FeedbackTests/FeedbackTests/FeedbackTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Faker;
+using ClassLibrary_Feedback;
+
+namespace FeedbackTests
+{
+    [TestClass]
+    public class FeedbackTest
+    {
+        [TestMethod]
+        public void Test_Feedback_Submition()
+        {
+            //arrange
+            string email = Internet.Email();
+            //act
+            NetworkManager sender = new NetworkManager();
+
+
+            //assert
+
+        }
+        public void Test_UserName_Lenght()
+        {
+            //arrange
+            //act
+            //assert
+        }
+        public void Test_UserName_NotEmpty()
+        {
+            //arrange
+            //act
+            //assert
+        }
+        public void Test_Email_IsValid()
+        {
+            //arrange
+            //act
+            //assert
+        }
+        public void Test_Raiting_Range()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}
---
+++ FeedbackTests/FeedbackTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FeedbackTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FeedbackTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd05babe-2279-461c-bdee-2f483ed1190c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
